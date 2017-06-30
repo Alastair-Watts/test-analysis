62ce1d6ab9e16890b14da84f03c88b7836901b68
Alastair <Alastair>
2017-06-30 23:34:55 +0100

    initial commit

---
+++ src/main/java/com/alastair/testanalysis/TestAnalysisApplication.java
@@ -0,0 +1,12 @@
+package com.alastair.testanalysis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestAnalysisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestAnalysisApplication.class, args);
+	}
+}
---
+++ src/test/java/com/alastair/testanalysis/TestAnalysisApplicationTests.java
@@ -0,0 +1,16 @@
+package com.alastair.testanalysis;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class TestAnalysisApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
